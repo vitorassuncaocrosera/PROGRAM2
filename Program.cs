f3de70040bef9158e6b9235ad1dac414ac21b139
VITOR <VITOR@VITORC>
2020-01-31 16:47:19 -0500

    commit all

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ Author: Vitor Assuncao Crosera
+ Date: 1/30/2020
+ Comments: This C# Console application code demonstrates the use of 
+           interation statements after getting input from users
+*/
+using System;
 
 namespace PROGRAM2
 {
@@ -15,25 +21,25 @@ namespace PROGRAM2
                 int value_of_input = int.Parse(input);
                 /* 
                    This conditional IF/ELSE IF/ELSE statement is used to test the values of user input.
-                   Depending on the value of the user input between 1 and 20, a different type of 
+                   Depending on the value of the user input between 1 and 100, a different type of 
                    iterative statement will execute. 
                 */
-                // If the value of user input is between 1 and 5, execute a For Loop
+                // If the value of user input is between 1 and 100, execute a For Loop
                 if ((value_of_input > 0) && (value_of_input <= 100)) 
                 {
                     // Use loopnum  the while loop while keeping number intact
-                    int loopNum = value_of_input;
+                    int loopNum = 1;
 
-                    // While loop that will loop through what the user has input. While loop will loop untill loopNum reaches 0
-                    while (loopNum > 0)
+                    // While loop that will loop through what the user has input. While loop will loop untill loopNum reaches the value
+                    while (loopNum <= value_of_input)
                     {
                         // writeline to display output
                         Console.WriteLine("You have entered: " + value_of_input + " This is the current integer value in the loop: " + loopNum);
-                        // decrease loopNet down by one to prevent infinite loop
-                        loopNum--;
+                        // decrease loopNet up by one to prevent infinite loop
+                        loopNum++;
                     }
                 }
-                else
+                else 
                 {
                     Console.WriteLine("Please enter an integer between 1 and 100 and try again ...");
                     Console.WriteLine("Press any key to exit the program and try again ...");
@@ -42,8 +48,8 @@ namespace PROGRAM2
             } // End of try
             catch
             {
-                Console.WriteLine("Please enter an integer value and try running the program again ...");
-                Console.WriteLine("Press any key to exit the program ...");
+                Console.WriteLine("Please enter an integer between 1 and 100 and try again ...");
+                Console.WriteLine("Please try again ...");
                 Console.ReadKey(true);
             } // End of catch
             } // End of Main
